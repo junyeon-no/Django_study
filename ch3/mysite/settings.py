aa414d87cb262c0b1143f37f31270961e7aa22f2
junhyun <whwns5203@naver.com>
2020-09-01 09:26:16 +0900

    add logComig and change from function view to class view

--- ch3/mysite/settings.py
+++ ch3/mysite/settings.py
@@ -123,3 +123,31 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGGING = {
+    'version':1,
+    'disable_existing_loggers' : False,
+    'formatters' : {
+        'verbose' : {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers':{
+        'file':{
+            'level' : 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'mysite.log'),
+            'formatter': 'verbose'
+        },
+    },
+        'loggers':{
+            'polls':{
+                'handlers':['file'],
+                'level': 'DEBUG',
+
+            },
+        },
+    
+}
+
+
